7e369247c806fd5d20f555bc9ba03d2d5148d2e0
Wagner Coleta <wagnercoleta@gmail.com>
2022-02-05 19:48:10 -0300

    add infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Wagner Coleta on 05/02/22.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+
+    func test_() throws {
+    }
+
+}

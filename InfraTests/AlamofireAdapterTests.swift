a60ecbfd008c9260bbd33bf5cfcf4cfb85176de8
Wagner Coleta <wagnercoleta@gmail.com>
2022-02-06 10:29:16 -0300

    move duplicated code to a helper method

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -25,24 +25,31 @@ class AlamofireAdapterTests: XCTestCase {
 
     func test_get_should_make_request_with_valid_url_and_method() {
         let url = makeUrl()
-        let sut = makeSut()
-        sut.get(to: url)
-        let exp = expectation(description: "waiting")
-        UrlProtocolStub.observeRequest { request in
+        testRequestFor(url: url) { request in
             XCTAssertEqual(url, request.url)
             XCTAssertEqual("GET", request.httpMethod)
-            exp.fulfill()
         }
-        wait(for: [exp], timeout: 1)
     }
 }
 
 extension AlamofireAdapterTests {
+    
     func makeSut() -> AlamofireAdapter {
         let configuration = URLSessionConfiguration.default
         configuration.protocolClasses = [UrlProtocolStub.self]
         let session = Session(configuration: configuration)
         return AlamofireAdapter(session: session)
+    }
+    
+    func testRequestFor(url: URL, action: @escaping (URLRequest) -> Void) {
+        let sut = makeSut()
+        sut.get(to: url)
+        let exp = expectation(description: "waiting")
+        UrlProtocolStub.observeRequest { request in
+            action(request)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
     }
 }
 

32f4ed88b955d85a2550abc621508a320daf68db
Wagner Coleta <wagnercoleta@gmail.com>
2022-02-19 18:26:39 -0300

    ensure ActiveViewController has a loading indicator and its hidden by default

---
+++ UI/ActiveViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ActiveViewController.swift
+//  UI
+//
+//  Created by Wagner Coleta on 19/02/22.
+//
+
+import Foundation
+import UIKit
+
+final class ActiveViewController: UIViewController {
+    
+    @IBOutlet weak var loadingIncator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

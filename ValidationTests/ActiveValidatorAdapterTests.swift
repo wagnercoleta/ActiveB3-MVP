18a3f67afd1c20a8e70e3190e0fd801aac2be794
Wagner Coleta <wagnercoleta@gmail.com>
2022-02-20 16:43:16 -0300

    add validation target

---
+++ ValidationTests/ActiveValidatorAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by Wagner Coleta on 20/02/22.
+//
+
+import XCTest
+
+class ActiveValidatorAdapterTests: XCTestCase {
+
+    func test_() {
+        
+    }
+}

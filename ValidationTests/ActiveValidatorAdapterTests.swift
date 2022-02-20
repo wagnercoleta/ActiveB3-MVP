87574934b7ecd3980c9ae48cd0ad3e12683536e6
Wagner Coleta <wagnercoleta@gmail.com>
2022-02-20 17:38:54 -0300

    ensure ActiveValidatorAdapter returns false if an invalid active is provided

--- ValidationTests/ActiveValidatorAdapterTests.swift
+++ ValidationTests/ActiveValidatorAdapterTests.swift
@@ -6,10 +6,28 @@
 //
 
 import XCTest
+import Presentation
+
+public final class ActiveValidatorAdapter: ActiveValidator {
+    
+    private let pattern = "([A-Z]{4}[0-9]{1})"
+    
+    public func isValid(active: String) -> Bool {
+        let range = NSRange(location: 0, length: active.utf16.count)
+        let regex = try! NSRegularExpression(pattern: pattern)
+        return regex.firstMatch(in: active, options: [], range: range) != nil
+    }
+}
 
 class ActiveValidatorAdapterTests: XCTestCase {
 
-    func test_() {
-        
+    func test_invalid_active() {
+        let sut = ActiveValidatorAdapter()
+        XCTAssertFalse(sut.isValid(active: ""))
+        XCTAssertFalse(sut.isValid(active: "123"))
+        XCTAssertFalse(sut.isValid(active: " xpt"))
+        XCTAssertFalse(sut.isValid(active: "P1234"))
+        XCTAssertFalse(sut.isValid(active: "PET32"))
+        XCTAssertFalse(sut.isValid(active: "XX22"))
     }
 }

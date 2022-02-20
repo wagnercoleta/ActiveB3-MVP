a5cdc9f57d5b0f2f42ecbef6cb813261b35258b2
Wagner Coleta <wagnercoleta@gmail.com>
2022-02-20 10:59:47 -0300

    move UIExtentions to a separate file

--- UITests/Scenes/Active/ActiveViewControllerTests.swift
+++ UITests/Scenes/Active/ActiveViewControllerTests.swift
@@ -47,17 +47,3 @@ extension ActiveViewControllerTests {
         return sut
     }
 }
-
-extension UIControl {
-    func simulate(event: UIControl.Event) {
-        allTargets.forEach { target in
-            actions(forTarget: target, forControlEvent: event)?.forEach({ action in
-                (target as NSObject).perform(Selector(action))
-            })
-        }
-    }
-    
-    func simulateTap() {
-        simulate(event: .touchUpInside)
-    }
-}

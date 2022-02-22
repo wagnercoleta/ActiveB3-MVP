bb3ff4151c87a21b6161c7f4ff5d533738dec88c
Wagner Coleta <wagnercoleta@gmail.com>
2022-02-21 21:16:38 -0300

    move ActiveValidatorAdapter to its own file

--- ValidationTests/ActiveValidatorAdapterTests.swift
+++ ValidationTests/ActiveValidatorAdapterTests.swift
@@ -6,21 +6,7 @@
 //
 
 import XCTest
-import Presentation
-
-public final class ActiveValidatorAdapter: ActiveValidator {
-    
-    private let pattern = "([A-Z]{4}[1-9]{1})"
-    
-    public func isValid(active: String) -> Bool {
-        if (active.count == 5) {
-            let range = NSRange(location: 0, length: active.utf16.count)
-            let regex = try! NSRegularExpression(pattern: pattern)
-            return regex.firstMatch(in: active, options: [], range: range) != nil
-        }
-        return false
-    }
-}
+import Validation
 
 class ActiveValidatorAdapterTests: XCTestCase {
 

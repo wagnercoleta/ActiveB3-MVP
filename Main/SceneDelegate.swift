6699278e4719a33376ca194687aaa4c254bbc6aa
Wagner Coleta <wagnercoleta@gmail.com>
2022-02-21 21:30:57 -0300

    add Main target

---
+++ Main/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Main
+//
+//  Created by Wagner Coleta on 21/02/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+

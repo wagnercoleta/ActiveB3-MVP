2a8ecfbaead17d40a44f85007cc40de523100d92
Wagner Coleta <wagnercoleta@gmail.com>
2022-02-26 09:25:17 -0300

    rename test file

--- MainTests/Composers/ActiveComposerTests.swift
+++ MainTests/Composers/ActiveComposerTests.swift
@@ -8,12 +8,9 @@
 import XCTest
 import Main
 
-class ActiveIntegrationTests: XCTestCase {
+class ActiveComposerTests: XCTestCase {
 
     func test_ui_presentation_integration_memoryleak() {
-        debugPrint("=================================")
-        debugPrint(Environment.variable(.apiBaseUrl))
-        debugPrint("=================================")
         let sut = ActiveComposer.composeControllerWith(readActive: ReadActiveSpy())
         checkMemoryLeak(for: sut)
     }

fd886950e6fa4c909467fc8a9d0e758710bf5f1d
Wagner Coleta <wagnercoleta@gmail.com>
2022-03-01 11:25:57 -0300

    add MainQueueDispatchDecorator execute on Main Thread

---
+++ Main/Decorators/MainQueueDispatchDecorator.swift
@@ -0,0 +1,65 @@
+//
+//  MainQueueDispatchDecorator.swift
+//  Main
+//
+//  Created by Wagner Coleta on 26/02/22.
+//
+
+import Foundation
+import Domain
+
+public final class MainQueueDispatchDecorator<T> {
+    private let instance: T
+    
+    public init(_ instance: T) {
+        self.instance = instance
+    }
+    
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else { return DispatchQueue.main.async {
+            completion()
+        }}
+        completion()
+    }
+}
+
+extension MainQueueDispatchDecorator: ReadActive where T: ReadActive {
+    public func read(readActiveModels: [ReadActiveModel], completion: @escaping (Result<[ActiveModel]?, DomainError>) -> Void) {
+        instance.read(readActiveModels: readActiveModels) { [weak self] result in
+            self?.dispatch {
+                completion(result)
+            }
+        }
+    }
+}
+/*
+public class BaseMainQueueDispatchDecorator<T> {
+    let instance: T
+    
+    public init(_ instance: T) {
+        self.instance = instance
+    }
+    
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else { return DispatchQueue.main.async {
+            completion()
+        }}
+        completion()
+    }
+}
+
+public final class RemoteReadActiveDecorator: BaseMainQueueDispatchDecorator<ReadActive> {
+    
+    public override init(_ instance: ReadActive){
+        super.init(instance)
+    }
+    
+    public func read(readActiveModels: [ReadActiveModel], completion: @escaping (Result<[ActiveModel]?, DomainError>) -> Void) {
+        instance.read(readActiveModels: readActiveModels) { result in
+            self.dispatch {
+                completion(result)
+            }
+        }
+    }
+}
+*/
